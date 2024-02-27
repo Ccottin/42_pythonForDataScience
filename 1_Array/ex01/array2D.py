0d7101e4f1229e710a676e4cfdd54a79b784b0fb
ccottin <cottin.clara7@gmail.com>
2024-02-27 20:19:47 +0100

    up

--- 1_Array/ex01/array2D.py
+++ 1_Array/ex01/array2D.py
@@ -2,4 +2,13 @@ import numpy as np
 
 
 def slice_me(family: list, start: int, end: int) -> list:
-    family = np.array(family)
+    try:
+        family = np.array(family)
+        print("My shape is :", family.shape)
+        
+    except AssertionError as e:
+        print("AssertionError: " + e.args[0] if e.args else '')
+        return (None)
+    except Exception as e:
+        print("Error: " + e.args[0] if e.args else '')
+        return (None)
