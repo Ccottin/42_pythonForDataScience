31f9afabf038de52079f2403f20652fa7e5c34dd
Clara COTTIN <ccottin@paul-f3Br9s3.clusters.42paris.fr>
2024-03-05 18:00:58 +0100

    day01 finished, but not relu.Moving onto day02

--- 1_Array/ex05/pimp_image.py
+++ 1_Array/ex05/pimp_image.py
@@ -51,28 +51,14 @@ def ft_blue(array) -> array:
         print("Error:", str(e))
 
 
-def rgb2gray(rgb):
-    """Outputs an rgb image into grayshades, without merging
-    channels."""
-    gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
-
-    return gray
-
-
 def ft_grey(array) -> array:
-    """Outputs the image received in grey shades.
-    It is based on Luma transform"""
+    """Outputs the image received in grey shades. Mean = moyenne"""
     try:
         assert array is not None, "No file provided."
-       # plt.imshow(array, cmap=plt.get_cmap('gray'))
-        grey = np.zeros(shape=array.shape, dtype=int)
-        grey[:, :, 0] = array[:, :, 0] / (1000/299)
-        grey[:, :, 1] = array[:, :, 1] / (1000/587)
-        grey[:, :, 2] = array[:, :, 2] / (1000/114)
-        plt.imshow(grey,  cmap=plt.get_cmap('gray'))
-        print("gr shape = ", grey.shape, grey)
-     #   gray = np.mean(array, -1)
-     #   plt.imshow(gray,  cmap=plt.get_cmap('gray'))
+        gray = np.zeros(shape=array.shape, dtype=int)
+        gray = np.mean(array, -1, dtype=int)
+        gray = np.repeat(gray[:, :, np.newaxis], 3, axis=2)
+        plt.imshow(gray,  cmap=plt.get_cmap('gray'))
         plt.show()
     except Exception as e:
         print("Error:", str(e))
