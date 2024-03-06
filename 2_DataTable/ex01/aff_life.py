24d77cc0d1c366d0966aae37c6ea7677235a6acb
Clara COTTIN <ccottin@paul-f4Br4s7.clusters.42paris.fr>
2024-03-06 21:27:11 +0100

    work in progress

--- 2_DataTable/ex01/aff_life.py
+++ 2_DataTable/ex01/aff_life.py
@@ -4,18 +4,21 @@ from load_csv import load
 
 def main():
     """This program will look for France's data into life_expetancy
-    csv, then display a graph representing it using matplotlib."""
+    csv, then display a graph representing it using matplotlib.
+    DataFrame.T is to transpose columns to rows and vice-versa.
+    DataFrame.plot() uses plt.plot() function."""
     try:
         data = load("life_expectancy_years.csv")
         assert data is not None, "No data provided"
-        life_data = data[data['France']] #itsabolutely not this
-        print(life_data)
-        time = data['country']
-        plt.plot(time, life_data)
-        plt.ylabel = 'life expectancy'
-        plt.xlabel = 'year'
-        plt.title = 'France Life expectancy Projections'
+        france_data = data[data['country'] == 'France'].set_index('country')
+        france_data = france_data.T
+        print(france_data)
+        france_data.plot(kind='line',
+                         title='France Life expectancy Projections',
+                         ylabel='life expectancy', xlabel='year', legend=False)
         plt.show()
+        plt.close()
+
     except Exception as e:
         print("Error:", str(e))
 
