3dd38e8f3a41e404f9af6f36d6a92a3e73d18529
Clara COTTIN <ccottin@paul-f3Br4s3.clusters.42paris.fr>
2024-05-09 14:51:15 +0200

    ex01 should be good

--- 3_OOP/ex01/S1E7.py
+++ 3_OOP/ex01/S1E7.py
@@ -1,43 +1,53 @@
 from S1E9 import Character
 
+# The term "<bound method xx.xx [...]>" indicates
+# that you're dealing with a method bound to a specific
+# instance of the xx class
+
 
 class Baratheon(Character):
+    """Representing the Baratheon family."""
+
     def __init__(self, first_name: str, is_alive=True,
                  eyes='brown', hairs='dark'):
         """Initialisation of the class"""
+
         self.first_name = first_name
         self.is_alive = is_alive
-        self.hairs = hairs
+        self.family_name = "Baratheon"
         self.eyes = eyes
-        self.familyname = "Baratheon"
-    
+        self.hairs = hairs
+
     def __str__(self):
         """returns a str."""
-        return f''
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
 
-  #  def __repr__(self):
-   #     """returns a str."""
-    #    return str(self)
+    def __repr__(self):
+        """returns a str."""
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
 
 
 class Lannister(Character):
+    """Representing the Lannister family."""
+
     def __init__(self, first_name: str, is_alive=True,
                  eyes='blue', hairs='light'):
         """Initialisation of the class"""
+        print("test =", first_name)
         self.first_name = first_name
         self.is_alive = is_alive
-        self.familyname = "Lannister"
+        self.family_name = "Lannister"
         self.eyes = eyes
         self.hairs = hairs
 
-    def create_lannister(self, first_name: str):
+    def create_lannister(first_name: str, is_alive: bool):
         """creates a new Lannister"""
         return Lannister(first_name)
 
     def __str__(self):
         """returns a str."""
-        return str(self)
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
 
-
-
-        
+    def __repr__(self):
+        """returns a str."""
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
