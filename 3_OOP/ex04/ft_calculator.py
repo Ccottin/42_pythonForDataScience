c0a05f3a67d1d8e9f3b2c279b1455dacafb627f1
ccottin <cottin.clara7@gmail.com>
2024-06-15 14:51:06 +0200

    it works but there are format issues

--- 3_OOP/ex04/ft_calculator.py
+++ 3_OOP/ex04/ft_calculator.py
@@ -1,16 +1,36 @@
 class calculator:
-"""Class calculating two vectors together"""
+    """Class calculating two vectors together"""
 
-    # decorator
+    # Decorators are used to modify the behavior of a function without
+    # changing it. Syntax can be 'def decorator(ft) :' then, later on,
+    # you can apply it to a function by adding '@decorator' above it
+
+    # The @staticmethod decorator is used to define static methods within a
+    # class. Static methods are the methods that can be called without creating
+    # an instance of the class. Static methods are often used when they don't
+    # have to access object-related parameters and are more related to the
+# class as a whole.
+
+    @staticmethod
     def dotproduct(V1: list[float], V2: list[float]) -> None:
-        print("coucou")
-        truc()
-    #your code here
-    # decorator
+        """Do a dot product of two vectors"""
+        dotProduct = 0
+        for i in V1:
+            dotProduct += i * V2[V1.index(i)]
+        print("Dot product is:", dotProduct)
+
+    @staticmethod
     def add_vec(V1: list[float], V2: list[float]) -> None:
-    #your code here
-    # decorator
+        """Will add two vectors"""
+        res = [0] * len(V1)
+        for i in V1:
+            res[V1.index(i)] = i + V2[V1.index(i)]
+        print("Add Vector is :", "{res:.1f}")
+
+    @staticmethod
     def sous_vec(V1: list[float], V2: list[float]) -> None:
-    
-    #https://medium.com/@marsdevs./everything-technical-with-decorators-in-python-76e27150840
-    
+        """Will substract two vectors"""
+        res = [0.0] * len(V1)
+        for i in V1:
+            res[V1.index(i)] = i - V2[V1.index(i)]
+        print("Sous Vector is :", "{res:.1f}")
